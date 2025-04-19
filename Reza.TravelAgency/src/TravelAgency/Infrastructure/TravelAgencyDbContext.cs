2b89edd2170fa7a58d17453216a5750e387bb6dd
Reza Hajizade <rezahajizade.tech@gmail.com>
2025-04-20 00:01:07 +0330

    Add Model to Project

---
+++ Reza.TravelAgency/src/TravelAgency/Infrastructure/TravelAgencyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TravelAgency.Models;
+
+namespace TravelAgency.Infrastructure
+{
+    public class TravelAgencyDbContext:IdentityDbContext<ApplicationUser>
+    {
+        public TravelAgencyDbContext(DbContextOptions<TravelAgencyDbContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
--- Reza.TravelAgency/src/TravelAgency/Program.cs
+++ Reza.TravelAgency/src/TravelAgency/Program.cs
@@ -1,5 +1,16 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TravelAgency.Infrastructure;
+using TravelAgency.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddDbContext<TravelAgencyDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("SvcDbContext")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<TravelAgencyDbContext>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
